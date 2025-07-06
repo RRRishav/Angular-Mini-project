e9ee5327d88ef57d460b0c5eef79f1106fc3117a
RRRishav <rishav6787@gmail.com>
2025-07-06 22:51:22 +0530

    Updated counter logic and fixed component import

--- src/app/app.ts
+++ src/app/app.ts
@@ -28,6 +28,15 @@ export class App {
  handleReset() {
   this.counter = 0
  }
- 
+
+ handleCounter(val:string){
+  if(val === 'increment'){
+    this.counter = this.counter  + 1
+  }else if(val === 'decrement'){
+    this.counter = this.counter  - 1
+  }else if(val === 'reset'){
+    this.counter = 0
+  }
+ }
 
 }
